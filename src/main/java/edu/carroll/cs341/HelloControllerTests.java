89b33ad0b205b9ea2f00d14310b18e4ecb0a6c7d
Jace Claassen <jclaassen@carroll.edu>
2025-08-24 15:59:46 -0600

    HelloControllerTest

--- src/main/java/edu/carroll/cs341/HelloControllerTests.java
+++ src/main/java/edu/carroll/cs341/HelloControllerTests.java
@@ -1,4 +1,4 @@
 package edu.carroll.cs341;
 
-public class HelloControllerTest {
+public class HelloControllerTests {
 }
--- src/test/java/edu/carroll/cs341/Cs341DemoApplicationTests.java
+++ src/test/java/edu/carroll/cs341/Cs341DemoApplicationTests.java
@@ -1,13 +1,26 @@
-package edu.carroll.cs341;
+ppackage edu.carroll.cs341;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class Cs341DemoApplicationTests {
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
-	void contextLoads() {
+	public void indexTest() throws Exception {
+		mockMvc.perform(get("/hello")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello from Spring Boot!")));
 	}
-
 }
+
