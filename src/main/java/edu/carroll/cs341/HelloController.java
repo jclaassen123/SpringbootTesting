406e85b7b451804305a82fa7e11ae39ddeebf72c
Jace Claassen <jclaassen@carroll.edu>
2025-08-24 15:25:19 -0600

    Controller for responding to root context requests

--- src/main/java/edu/carroll/cs341/HelloController.java
+++ src/main/java/edu/carroll/cs341/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String index() {
-        return "Hello from Spring Boot!";
+        return "Greetings from Spring Boot!";
     }
 }
