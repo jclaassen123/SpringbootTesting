bce59c1e4ee52ac9297233e24416adc38f8b0469
Jace Claassen <jclaassen@carroll.edu>
2025-08-24 15:37:55 -0600

    Update route and changed output

--- src/main/java/edu/carroll/cs341/HelloController.java
+++ src/main/java/edu/carroll/cs341/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Big hello from Spring Boot!";
     }
 }
