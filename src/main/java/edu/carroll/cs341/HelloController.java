11b80c518089e368b591066e7d48f48b8a356e60
Jace Claassen <jclaassen@carroll.edu>
2025-08-24 16:33:47 -0600

    Specific greeting with html

--- src/main/java/edu/carroll/cs341/HelloController.java
+++ src/main/java/edu/carroll/cs341/HelloController.java
@@ -1,12 +1,16 @@
 package edu.carroll.cs341;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // <- correct Model import
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
-    public String index() {
-        return "index";
+    @GetMapping("/")
+    public String index(@RequestParam(value="name", required=false, defaultValue="Student") String name,
+                        Model model) {
+        model.addAttribute("name", name);
+        return "index"; // corresponds to src/main/resources/templates/index.html
     }
 }
