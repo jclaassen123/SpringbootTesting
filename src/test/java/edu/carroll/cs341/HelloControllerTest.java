b6ef3eb2f4115c0dc762be51678fa568efa145e5
Jace Claassen <jclaassen@carroll.edu>
2025-08-24 16:02:22 -0600

    stuff

--- src/test/java/edu/carroll/cs341/HelloControllerTest.java
+++ src/test/java/edu/carroll/cs341/HelloControllerTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
 @WebMvcTest(HelloController.class)
 public class HelloControllerTest {
     @Autowired
