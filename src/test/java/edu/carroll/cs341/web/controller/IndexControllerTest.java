c14216ef08f69226c32cc3f445dda483fb07194d
Jace Claassen <jclaassen@carroll.edu>
2025-08-25 10:27:27 -0600

    Login Testing

--- src/test/java/edu/carroll/cs341/web/controller/IndexControllerTest.java
+++ src/test/java/edu/carroll/cs341/web/controller/IndexControllerTest.java
@@ -1,6 +1,7 @@
-package edu.carroll.cs341;
+package edu.carroll.cs341.web.controller;
 
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.containsString;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -10,15 +11,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-@WebMvcTest(HelloController.class)
-public class HelloControllerTest {
+
+@WebMvcTest(IndexController.class)
+public class IndexControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
     @Test
     public void indexTest() throws Exception {
-        mockMvc.perform(get("/hello")).andDo(print())
+        mockMvc.perform(get("/")).andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Hello from Spring Boot!")));
+                .andExpect(content().string(containsString("Welcome to the application!")))
+                .andExpect(content().string(containsString("login")));
     }
 }
